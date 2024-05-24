fd03a1f66b9a0488104910a95f280b3205296255
Артем Еремеев <jeremy1002@yandex.ru>
2024-05-24 22:35:53 +0300

    chore: initial commit

---
+++ ipa.scanner/IpaScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IpaScannerApp.swift
+//  ipa.scanner
+//
+//  Created by Artem Eremeev on 24.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct IpaScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
