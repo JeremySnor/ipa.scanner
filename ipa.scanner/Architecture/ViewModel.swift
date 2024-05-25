6b500024b2647fda58dc22f3ef21884efff7ccbd
Артем Еремеев <jeremy1002@yandex.ru>
2024-05-25 23:35:24 +0300

    feat: add actions and error processing

--- ipa.scanner/Architecture/ViewModel.swift
+++ ipa.scanner/Architecture/ViewModel.swift
@@ -7,12 +7,29 @@
 
 import Foundation
 
-class ViewModel: ObservableObject {
+class ViewModel<Action>: ObservableObject {
     
     let navigationContext: NavigationContext
     
     init(navigationContext: NavigationContext) {
         self.navigationContext = navigationContext
     }
+    
+    @MainActor
+    func updateBinding(_ update: () -> Void) {
+        update()
+    }
+    
+    func handleAction(_ action: Action) {
+        Task {
+            do {
+                try await processAction(action)
+            } catch {
+                await navigationContext.showError(error)
+            }
+        }
+    }
+    
+    func processAction(_ action: Action) async throws { }
     
 }
