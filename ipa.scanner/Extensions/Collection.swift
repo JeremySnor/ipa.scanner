030da36bd34e1d2a0f5b56684c54200aefdc54ed
Артем Еремеев <jeremy1002@yandex.ru>
2024-05-26 02:34:01 +0300

    feat: add extension for collection

---
+++ ipa.scanner/Extensions/Collection.swift
@@ -0,0 +1,16 @@
+//
+//  Collection.swift
+//  ipa.scanner
+//
+//  Created by Artem Eremeev on 26.05.2024.
+//
+
+import Foundation
+
+extension Collection {
+    
+    var isNotEmpty: Bool {
+        !isEmpty
+    }
+    
+}
